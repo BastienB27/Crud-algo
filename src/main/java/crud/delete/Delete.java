9a5c3230c1d7adb419af084d8e9a4169f93d80f9
Floria <florian.richet2004@gmail.com>
2023-01-26 12:41:23 +0100

    Country/City/film + Edit en précisant les options

--- src/main/java/crud/delete/Delete.java
+++ src/main/java/crud/delete/Delete.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Delete {
     public static void Delete() throws SQLException {
     }
-    static String country_name;
+    static String name;
     static boolean ajout;
 
     public static boolean Supprimer_country() throws SQLException {
@@ -17,7 +17,7 @@ public class Delete {
         try {
             Scanner console = new Scanner(System.in);
             System.out.print("Entrer le nom du pays à supprimer: ");
-            country_name = console.nextLine();
+            name = console.nextLine();
 
 
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -31,7 +31,7 @@ public class Delete {
             PreparedStatement prepare = conn.prepareStatement(sql);
 
             //Variable country_name
-            prepare.setString(1,country_name);
+            prepare.setString(1,name);
 
 
             prepare.executeUpdate();
@@ -46,6 +46,94 @@ public class Delete {
             System.exit(0);
         }
         System.out.println("\u001B[32m+++ Pays supprimé");
+        System.exit(0);
+        return true;
+
+
+    }
+
+
+    public static boolean Supprimer_city() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+
+        try {
+            Scanner console = new Scanner(System.in);
+            System.out.print("Entrer le nom de la ville à supprimer: ");
+            name = console.nextLine();
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("\u001B[36m/// Connexion établie!");
+
+            //Requete d'insertion
+            String sql = "DELETE INTO city WHERE city = ?";
+
+            //Créer l'objet statement
+            PreparedStatement prepare = conn.prepareStatement(sql);
+
+            //Variable country_name
+            prepare.setString(1,name);
+
+
+            prepare.executeUpdate();
+
+
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("\u001B[31m---Erreur, données invalides");
+            System.out.println("\u001B[31mVous avez rentré une valeur incorrecte ou");
+            System.out.println("\u001B[31mCette ville est relié à une clef étrangère et, donc, pas supprimable");
+            System.exit(0);
+        }
+        System.out.println("\u001B[32m+++ Ville supprimé");
+        System.exit(0);
+        return true;
+
+
+    }
+
+
+    public static boolean Supprimer_film() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+
+        try {
+            Scanner console = new Scanner(System.in);
+            System.out.print("Entrer le nom du film à supprimer: ");
+            name = console.nextLine();
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("\u001B[36m/// Connexion établie!");
+
+            //Requete d'insertion
+            String sql = "DELETE INTO film WHERE film = ?";
+
+            //Créer l'objet statement
+            PreparedStatement prepare = conn.prepareStatement(sql);
+
+            //Variable country_name
+            prepare.setString(1,name);
+
+
+            prepare.executeUpdate();
+
+
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("\u001B[31m---Erreur, données invalides");
+            System.out.println("\u001B[31mVous avez rentré une valeur incorrecte ou");
+            System.out.println("\u001B[31mCe film est relié à une clef étrangère et, donc, pas supprimable");
+            System.exit(0);
+        }
+        System.out.println("\u001B[32m+++ Film supprimé");
         System.exit(0);
         return true;
 
--- src/main/java/crud/show/Data.java
+++ src/main/java/crud/show/Data.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Data {
     public static String nom_table;
     public static String recherche;
+    static boolean search = true;
 
     public static void Data_table_country() {
 
@@ -32,8 +33,11 @@ public class Data {
                     }
                     System.out.println("");
                 }
-                Recherche_table_country();
+                if (search == true) {
+                    Recherche_table_country();
+                } else {
 
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -58,6 +62,166 @@ public class Data {
             recherche = console.nextLine();
 
             String sql = "SELECT * FROM " + tableName + " WHERE country = ?";
+
+            //Créer l'objet statement
+            PreparedStatement prepare = conn.prepareStatement(sql);
+
+            //Variable country_name
+            prepare.setString(1, recherche);
+
+            //Executer la requête
+            ResultSet res = prepare.executeQuery();
+
+            //Afficher les résultats
+            ResultSetMetaData rsmd = res.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (res.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = res.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void Data_table_city() {
+
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+        String tableName = "city";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("/// Connexion établie!");
+
+            // Sélection des données de la table
+            Statement pstmt = conn.createStatement();
+            ResultSet res = pstmt.executeQuery("SELECT * FROM " + tableName);
+            ResultSetMetaData rsmd = res.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (res.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = res.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+            Recherche_table_city();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+
+
+    }
+    public static void Recherche_table_city() {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+        String tableName = "city";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("/// Connexion établie!");
+
+            Scanner console = new Scanner(System.in);
+            System.out.print("Entrer le nom de la ville que vous voulez rechercher: ");
+            recherche = console.nextLine();
+
+            String sql = "SELECT * FROM " + tableName + " WHERE city = ?";
+
+            //Créer l'objet statement
+            PreparedStatement prepare = conn.prepareStatement(sql);
+
+            //Variable country_name
+            prepare.setString(1, recherche);
+
+            //Executer la requête
+            ResultSet res = prepare.executeQuery();
+
+            //Afficher les résultats
+            ResultSetMetaData rsmd = res.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (res.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = res.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void Data_table_film() {
+
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+        String tableName = "film";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("/// Connexion établie!");
+
+            // Sélection des données de la table
+            Statement pstmt = conn.createStatement();
+            ResultSet res = pstmt.executeQuery("SELECT * FROM " + tableName);
+            ResultSetMetaData rsmd = res.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (res.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = res.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+            Recherche_table_film();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+
+
+    }
+    public static void Recherche_table_film() {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String username = "root";
+        String password = "";
+        String tableName = "film";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("/// Connexion établie!");
+
+            Scanner console = new Scanner(System.in);
+            System.out.print("Entrer le nom du film que vous voulez rechercher: ");
+            recherche = console.nextLine();
+
+            String sql = "SELECT * FROM " + tableName + " WHERE film = ?";
 
             //Créer l'objet statement
             PreparedStatement prepare = conn.prepareStatement(sql);
